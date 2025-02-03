c8355221a9fe050faed6d54bb867bfce6cdd7ade
lrtygol <Ignat.merz@gmail.com>
2025-02-03 20:06:31 +0700

    small update

--- Assets/scripts/ENEMY.cs
+++ Assets/scripts/ENEMY.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class ENEMY : MonoBehaviour
 {
     public Transform player;
-    
+    public int speed;
 
 
 
     void Update()
     {
         transform.LookAt(player);
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
     
 }
